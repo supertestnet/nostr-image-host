f46a9194a8beb71d32d2754ab907bfc730d037f8
supertestnet <58400631+supertestnet@users.noreply.github.com>
2024-01-06 16:50:26 -0600

    Update nostr_image_host.js

--- nostr_image_host.js
+++ nostr_image_host.js
@@ -1,46 +1,107 @@
 var nostr_image_host = {
-	getImageFromImageId: async image_id => {
-	    var relay_hex = image_id.substring( 64 );
-	    var relay = nostr_image_host.hexToText( relay_hex );
-	    var file_id = image_id.substring( 0, 64 );
-	    var thing_i_need = await nostr_image_host.load_file( file_id, relay );
-	    var image_b64 = await nostr_image_host.getNote( `nostr_image_${thing_i_need}` );
-	    sessionStorage.removeItem( `nostr_image_${thing_i_need}` );
-	    return image_b64;
-	},
-	getNote: async item => {
-	    async function isNoteSetYet( note_i_seek ) {
-	        return new Promise( function( resolve, reject ) {
-	            if ( !note_i_seek ) {
-	                setTimeout( async function() {
-	                    var msg = await isNoteSetYet( sessionStorage[ item ] );
-	                    resolve( msg );
-	                }, 100 );
-	            } else {
-	                resolve( note_i_seek );
-	            }
-	        });
-	    }
-	    async function getTimeoutData() {
-	        var note_i_seek = await isNoteSetYet( sessionStorage[ item ] );
-	        return note_i_seek;
-	    }
-	    var returnable = await getTimeoutData();
-	    return returnable;
-	},
-	textToHex: text => {
-	    var encoder = new TextEncoder().encode( text );
-	    return [...new Uint8Array(encoder)]
-	        .map( x => x.toString( 16 ).padStart( 2, "0" ) )
-	        .join( "" );
-	},
-	hexToText: hex => {
-	    var bytes = new Uint8Array(Math.ceil(hex.length / 2));
-	    for (var i = 0; i < hex.length; i++) bytes[i] = parseInt(hex.substr(i * 2, 2), 16);
-	    var text = new TextDecoder().decode( bytes );
-	    return text;
-	},
-	load_file: ( file_id, relay ) => {
+    percent_done_uploading: "0%",
+    percent_done_downloading: "0%",
+    whole_id: null,
+    upload_data: [],
+    hexToBytes: hex => Uint8Array.from( hex.match( /.{1,2}/g ).map( ( byte ) => parseInt( byte, 16 ) ) ),
+    bytesToHex: bytes => bytes.reduce( ( str, byte ) => str + byte.toString( 16 ).padStart( 2, "0" ), "" ),
+    textToHex: text => {
+        var encoder = new TextEncoder().encode( text );
+        return [...new Uint8Array(encoder)]
+            .map( x => x.toString( 16 ).padStart( 2, "0" ) )
+            .join( "" );
+    },
+    hexToText: hex => {
+        var bytes = new Uint8Array(Math.ceil(hex.length / 2));
+        for (var i = 0; i < hex.length; i++) bytes[i] = parseInt(hex.substr(i * 2, 2), 16);
+        var text = new TextDecoder().decode( bytes );
+        return text;
+    },
+    encodeBase64: file => {
+        return new Promise( function( resolve, reject ) {
+            var imgReader = new FileReader();
+            imgReader.onloadend = function() {
+                resolve( imgReader.result.toString() );
+            }
+            imgReader.readAsDataURL( file );
+        });
+    },
+    waitSomeSeconds: num => {
+        var num = num.toString() + "000";
+        num = Number( num );
+        return new Promise( resolve => setTimeout( resolve, num ) );
+    },
+    getNote: async item => {
+        async function isNoteSetYet( note_i_seek ) {
+            return new Promise( function( resolve, reject ) {
+                if ( !note_i_seek ) {
+                    setTimeout( async function() {
+                        var msg = await isNoteSetYet( sessionStorage[ item ] );
+                        resolve( msg );
+                    }, 100 );
+                } else {
+                    resolve( note_i_seek );
+                }
+            });
+        }
+        async function getTimeoutData() {
+            var note_i_seek = await isNoteSetYet( sessionStorage[ item ] );
+            return note_i_seek;
+        }
+        var returnable = await getTimeoutData();
+        return returnable;
+    },
+    getSignedEvent: async (event, privateKey) => {
+        var eventData = JSON.stringify([
+            0,                  // Reserved for future use
+            event['pubkey'],        // The sender's public key
+            event['created_at'],    // Unix timestamp
+            event['kind'],      // Message “kind” or type
+            event['tags'],      // Tags identify replies/recipients
+            event['content']        // Your note contents
+        ])
+        event.id  = nostr_image_host.bytesToHex( sha256( eventData ) );
+        event.sig = await nobleSecp256k1.schnorr.sign( event.id, privateKey );
+        return event;
+    },
+    sendNoteAndReturnId: async ( note, part, whole, socket, privKey, pubKey ) => {
+        var event = {
+            "content"    : note,
+            "created_at" : Math.floor( Date.now() / 1000 ),
+            "kind"       : 57009,
+            "tags"       : [ [ "pieces", `${part} of ${whole}` ] ],
+            "pubkey"     : pubKey,
+        }
+        var signedEvent = await nostr_image_host.getSignedEvent( event, privKey );
+        socket.send(JSON.stringify([ "EVENT", signedEvent ]));
+        return signedEvent.id;
+    },
+    uploadToNostr: async ( file, relay ) => {
+        return new Promise( async ( resolve, reject ) => {
+            if ( !relay ) return;
+            if ( !relay.startsWith( "wss://" ) ) relay = "wss://" + relay;
+            var b64 = await nostr_image_host.encodeBase64( file );
+            var socket = new WebSocket( relay );
+            socket.addEventListener('open', async function( e ) {
+                var array = b64.match(/.{1,4000}/g);
+                var privKey = nostr_image_host.bytesToHex( nobleSecp256k1.utils.randomPrivateKey() );
+                var pubKey = nobleSecp256k1.getPublicKey( privKey, true ).substring( 2 );
+                nostr_image_host.percent_done_uploading = "0%";
+                var i; for ( i=0; i<array.length; i++ ) {
+                    var note = array[ i ];
+                    var part = i + 1;
+                    var whole = array.length;
+                    var id = await nostr_image_host.sendNoteAndReturnId( note, part, whole, socket, privKey, pubKey );
+                    var percent = Number( ( ( part / whole ) * 100 ).toFixed( 2 ) );
+                    nostr_image_host.percent_done_uploading = percent + "%";
+                    if ( percent == 100 ) resolve( id + nostr_image_host.textToHex( relay ) );
+                    await nostr_image_host.waitSomeSeconds( 2 );
+                }
+                socket.close();
+            });
+        });
+    },
+    load_file: ( file_id, relay ) => {
         return new Promise( function( resolve, reject ) {
             var pieces = [];
             var socket = new WebSocket( relay );
@@ -88,74 +149,13 @@ var nostr_image_host = {
             });
         });
     },
-    getSignedEvent: async (event, privateKey) => {
-        var eventData = JSON.stringify([
-            0,                  // Reserved for future use
-            event['pubkey'],        // The sender's public key
-            event['created_at'],    // Unix timestamp
-            event['kind'],      // Message “kind” or type
-            event['tags'],      // Tags identify replies/recipients
-            event['content']        // Your note contents
-        ])
-        event.id  = nostr_image_host.bytesToHex( sha256( eventData ) );
-        event.sig = await nobleSecp256k1.schnorr.sign( event.id, privateKey );
-        return event;
-    },
-    hexToBytes: hex => Uint8Array.from( hex.match( /.{1,2}/g ).map( ( byte ) => parseInt( byte, 16 ) ) ),
-    bytesToHex: bytes => bytes.reduce( ( str, byte ) => str + byte.toString( 16 ).padStart( 2, "0" ), "" ),
-    whole_id: null,
-    upload_data: [],
-    waitSomeSeconds: num => {
-        var num = num.toString() + "000";
-        num = Number( num );
-        return new Promise( resolve => setTimeout( resolve, num ) );
-    },
-    uploadToNostr: async ( file, relay ) => {
-        return new Promise( async ( resolve, reject ) => {
-            if ( !relay ) return;
-            if ( !relay.startsWith( "wss://" ) ) relay = "wss://" + relay;
-            var b64 = await nostr_image_host.encodeBase64( file );
-            var socket = new WebSocket( relay );
-            socket.addEventListener('open', async function( e ) {
-                var array = b64.match(/.{1,4000}/g);
-                var privKey = nostr_image_host.bytesToHex( nobleSecp256k1.utils.randomPrivateKey() );
-                var pubKey = nobleSecp256k1.getPublicKey( privKey, true ).substring( 2 );
-                nostr_image_host.percent_done_uploading = "0%";
-                var i; for ( i=0; i<array.length; i++ ) {
-                    var note = array[ i ];
-                    var part = i + 1;
-                    var whole = array.length;
-                    var id = await nostr_image_host.sendNoteAndReturnId( note, part, whole, socket, privKey, pubKey );
-                    var percent = Number( ( ( part / whole ) * 100 ).toFixed( 2 ) );
-                    nostr_image_host.percent_done_uploading = percent + "%";
-                    if ( percent == 100 ) resolve( id + nostr_image_host.textToHex( relay ) );
-                    await nostr_image_host.waitSomeSeconds( 2 );
-                }
-                socket.close();
-            });
-        });
-    },
-    encodeBase64: file => {
-        return new Promise( function( resolve, reject ) {
-            var imgReader = new FileReader();
-            imgReader.onloadend = function() {
-                resolve( imgReader.result.toString() );
-            }
-            imgReader.readAsDataURL( file );
-        });
-    },
-    sendNoteAndReturnId: async ( note, part, whole, socket, privKey, pubKey ) => {
-        var event = {
-            "content"    : note,
-            "created_at" : Math.floor( Date.now() / 1000 ),
-            "kind"       : 57009,
-            "tags"       : [ [ "pieces", `${part} of ${whole}` ] ],
-            "pubkey"     : pubKey,
-        }
-        var signedEvent = await nostr_image_host.getSignedEvent( event, privKey );
-        socket.send(JSON.stringify([ "EVENT", signedEvent ]));
-        return signedEvent.id;
-    },
-    percent_done_uploading: "0%",
-    percent_done_downloading: "0%"
+    getImageFromImageId: async image_id => {
+        var relay_hex = image_id.substring( 64 );
+        var relay = nostr_image_host.hexToText( relay_hex );
+        var file_id = image_id.substring( 0, 64 );
+        var thing_i_need = await nostr_image_host.load_file( file_id, relay );
+        var image_b64 = await nostr_image_host.getNote( `nostr_image_${thing_i_need}` );
+        sessionStorage.removeItem( `nostr_image_${thing_i_need}` );
+        return image_b64;
+    }
 }
